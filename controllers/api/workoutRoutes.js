eb3201166cda50fc6af37be1d7dc7a0adc984f2f
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-02 22:30:50 +1000

    Add workouts/range Route

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,14 +1,25 @@
 const router = require("express").Router();
 const Workout  = require("../../models/Workout.js");
 
-// router.get("/", async (req, res) => {
-//     try {
-//     const allWorkouts = await Workout.find({});
-//     res.json(allWorkouts)   
-// } catch (err) {
-//     res.status(500).json(err);
-//   }
-// })  
+router.get("/range", async (req, res) => {
+    try {
+    const allWorkouts = await Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                },
+            },
+        },
+        {
+            $sort: { day: 1 }  
+        }
+    ]);
+    res.status(200).json(allWorkouts)   
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 router.get("/", async (req, res) => {
     try{
@@ -25,7 +36,8 @@ router.get("/", async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
       }
-})  
+});
+
 
 
 module.exports = router;
