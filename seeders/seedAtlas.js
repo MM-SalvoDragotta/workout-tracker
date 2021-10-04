aa42920e5284547c972835a7932a9949b66ab94b
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-04 16:54:22 +1100

    Add env variable to seed Atlas DB

---
+++ seeders/seedAtlas.js
@@ -0,0 +1,141 @@
+const MongoClient = require("mongodb").MongoClient;
+require('dotenv').config();
+
+const workoutSeed = [
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 9)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Bicep Curl',
+        duration: 20,
+        weight: 100,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 8)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Lateral Pull',
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 7)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Push Press',
+        duration: 25,
+        weight: 185,
+        reps: 8,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 6)),
+    exercises: [
+      {
+        type: 'cardio',
+        name: 'Running',
+        duration: 25,
+        distance: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 5)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Bench Press',
+        duration: 20,
+        weight: 285,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 4)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Bench Press',
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 3)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Quad Press',
+        duration: 30,
+        weight: 300,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 2)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Bench Press',
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 1)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Military Press',
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+];
+
+async function seedDB() {
+  // Connection URL
+  const uri = process.env.URI;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  });
+
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const collection = await client.db("workoutsDB").collection("workouts");
+    await collection.insertMany(workoutSeed);
+    console.log("Database seeded! :)");
+    client.close();
+} catch (err) {
+    console.log(err.stack);
+}
+}
+seedDB();
